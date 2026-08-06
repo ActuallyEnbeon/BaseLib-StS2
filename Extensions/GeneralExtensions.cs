fa5103c8281bff728adae94d900d996906ee1b06
Alchyr <isithalchyr@gmail.com>
2026-08-05 17:09:52 -0700

    fix custom pile patch for beta

---
+++ Extensions/GeneralExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace BaseLib.Extensions;
+
+internal static class GeneralExtensions
+{
+    internal static T LogChained<T>(this T obj, string logText)
+    {
+        BaseLibMain.Logger.Info(logText);
+        return obj;
+    }
+    
+    internal static T Chained<T>(this T obj, Action<T> action)
+    {
+        action(obj);
+        return obj;
+    }
+    
+    internal static T ChainedGet<T, U>(this T obj, Func<T, U> getter, out U result)
+    {
+        result = getter(obj);
+        return obj;
+    }
+}
